2dc9a06a99737cf6a181accaf734f25016d0b5a7
Sanjay Shirsath <sdshirsath@Sanjays-MacBook-Air.local>
2022-06-25 19:45:54 +0530

    Initial commit

---
+++ src/Conversion.kt
@@ -0,0 +1,25 @@
+fun main(args:Array<String>) {
+    /*
+    var i = 10
+    val j: Float = i.toFloat()      // Conversion from Int to Float
+    println(j)
+     */
+
+    /*
+    var i = 90.75
+    val j: Int = i.toInt()          // Conversion from Float to Int
+    println(j)
+     */
+
+    /*
+    var i = 20
+    val j: String = i.toString()   // Conversion from Int to String
+    println(j)
+         */
+
+    var i: Int = 10
+    val j: Long = i.toLong()      // Conversion from Int to Loang
+    println(j)
+
+    println("Fourth Kotline Prog , By Sanjay DEVIDAS Shirsath")
+}
